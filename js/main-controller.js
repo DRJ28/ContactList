5c670c1a0f9615617164b5b4f3915563ca29ed21
DRJ28 <dheerajmehta10275@gmail.com>
2016-02-09 00:32:18 +0530

    localstorage

--- js/main-controller.js
+++ js/main-controller.js
@@ -1,21 +1,25 @@
 var module = angular.module('contactList',[]);
 module.controller('maincontroller',['$scope', function($scope){
-	$scope.contactlist = [
-		{
-			"name": "Dheeraj",
-			"number": "+918275755512"
-		}
-	]
+	$scope.contactlist = [];
+	/*if (localStorage.getItem('list')) {
+		var list = localStorage.getItem('list')
+		$scope.contactlist = JSON.parse(list);
+	};*/
 	$scope.addName = '';
 	$scope.addNumber = '';
 	$scope.addToList = function(){
+		debugger;
 		console.log($scope.addName, $scope.addNumber);
 		var obj = {
 			"name": $scope.addName,
 			"number": $scope.addNumber
 		}
 		if ($scope.addName != '' && $scope.addNumber != '') {
-			$scope.contactlist.push(obj);	
+			$scope.contactlist.push(obj);
+			/*chrome.storage.local.set({'name': 787687677575}, function(){
+				alert('stored');
+			});*/
+			localStorage.setItem('list', $scope.contactlist)	
 			$scope.addName = '';
 			$scope.addNumber = '';
 		};
